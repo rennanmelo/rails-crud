2ff7331ff57553dd1fd331a93ec851fdbce2a34b
Rennan Melo <rennan.melo11@gmail.com>
2019-12-17 20:41:00 -0200

    List all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def show
         @article = Article.find(params[:id])
     end
@@ -15,6 +19,6 @@ class ArticlesController < ApplicationController
 
     private
         def article_params
-        params.require(:article).permit(:title, :text)
-    end
+            params.require(:article).permit(:title, :text)
+        end
 end
